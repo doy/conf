a037f1227765630a7f01c75515117e1511157f7c
Jesse Luehrs <doy@tozt.net>
2017-10-20 03:45:52 -0400

    more random config cleanups

--- fish/functions/fish_prompt.fish
+++
@@ -1,3 +0,0 @@
-function fish_prompt
-    ~/.bin/fancy-prompt $status
-end
--- fish/functions/perldoc.fish
+++
@@ -1,7 +0,0 @@
-function perldoc
-    if type cpandoc > /dev/null 2>&1
-        cpandoc $argv
-    else
-        command perldoc $argv
-    end
-end
