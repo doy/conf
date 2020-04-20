6efd5fe5afd46e8623d70201e8907eb06372eb7a
Jesse Luehrs <doy@tozt.net>
2020-04-20 05:42:15 -0400

    switch to rbw

--- config/offlineimap/pass.py
+++ config/offlineimap/pass.py
@@ -1,5 +1,5 @@
 from subprocess import Popen, PIPE
 
-def get_password(key):
-    (out, err) = Popen(["pass", key], stdout=PIPE).communicate()
+def get_password(name, user):
+    (out, err) = Popen(["rbw", "get", name, user], stdout=PIPE).communicate()
     return out.strip()
